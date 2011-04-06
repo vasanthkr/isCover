6b0553af07a2b76b2a2765edbcdbbf272c274e1b
Vasanth Kumar Rajendran <mail@vasanthkr.com>
2011-04-06 00:01:16 -0700

    Added a function to compute TAR coefficients.

---
+++ modelTAR.m
@@ -0,0 +1,21 @@
+function [As medoids] = modelTAR(S_a, S_x, K)
+%MODELTAR Find TAR coefficients for a given time series
+%   S_a(D x N') : time series of descriptors.
+%   S_x(MD x N'): time series of history-concatenated descriptors.
+%                 (where N' = N - w - h)
+%   K           : number of clusters
+
+%% Cluster columns of S_x into K clusters
+[label, ~, medIndex] = kmedoids(S_x, K);
+
+%% Find AR clusters for each clusters
+As = zeros(size(S_a, 1), size(S_x, 1), K);
+for k = 1:K
+    As(:, :, k) = (S_x(:,(label==k))' \ S_a(:, (label==k))')';
+end
+
+%% Filter the K medoids
+medoids = S_x(:, medIndex);
+
+end
+
